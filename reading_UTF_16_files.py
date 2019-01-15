0537f55d78f8114e984ce55a2947221c550252fd
Malyaj Srivastav <Malyaj@users.noreply.github.com>
2019-01-15 15:31:39 +0530

    Create reading_UTF_16_files.py
    Reading files which open in excel with a file format warning - presumably these are in UTF 16 format
    
    Opening these files using codecs library, parsing the data manually into a pandas dataframe, and saving it as csv or excel file

---
+++ reading_UTF_16_files.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import codecs
+
+path = r"D:\Users\703143501\Documents\GE Confidential\WIP\2019\weekly_reports\Metric report FW 03\data\cdsi.xls"
+
+fp= codecs.open(path, "r", "utf-16")
+lines= fp.readlines()
+
+header_row_location = 2
+tab = '\t'
+
+for line_number in range(len(lines)):
+    if line_number < 2:
+        continue
+    elif line_number == 2:
+        headers = lines[line_number].split(tab)[:-2]
+        df = pd.DataFrame(columns = headers)
+    else:
+        # insert row
+        row = lines[line_number].split(tab)[:-2]
+        df = df.append(pd.Series(row, index=df.columns ), ignore_index=True)
+        #df.append(pd.Series(row, index=df.columns ), ignore_index=True)
+
+# export this df to an excel file
+path_to_save = r"D:\Users\703143501\Documents\GE Confidential\WIP\2019\weekly_reports\Metric report FW 03\data\cdsi.csv"
+df.to_csv(path_to_save, index = False)
